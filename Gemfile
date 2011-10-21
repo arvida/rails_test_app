6796d50ef6f05e3b5bfdaee975ce342debc2514e
Arvid Andersson <arvid@winstondesign.se>
2011-10-21 15:53:26 +0200

    Init commit

---
+++ Gemfile
@@ -0,0 +1,16 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.1.0'
+
+gem 'unicorn'
+
+# Deploy with Capistrano
+# gem 'capistrano'
+
+# To use debugger
+# gem 'ruby-debug19', :require => 'ruby-debug'
+
+group :test do
+  # Pretty printed test output
+  gem 'turn', :require => false
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsTestApp::Application.config.secret_token = '7e92e93e9324d0c48d5e16f0a865793ddf7ce8fdfa5da5d22981c565f1c55b843273af45c921e0dc2aa72ca295d9e01c6ca245a3f2c6ac3eecb83e6143423b69'
